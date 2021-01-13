8069519605fee2fa2022b659ba9f9775bb9d383a
Daniel Lovell <lovell.daniel92@gmail.com>
2021-01-12 23:57:27 -0800

    Initial implementation of the mod

---
+++ SlowDown/Patches.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Assets.Scripts;
+using Assets.Scripts.Voxel;
+using HarmonyLib;
+
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+using Assets.Scripts.Objects.Items;
+using System.Reflection.Emit;
+using Assets.Scripts.GridSystem;
+using HarmonyLib.Tools;
+using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Electrical;
+
+namespace SlowDown
+{
+
+    /// <summary>
+    /// Alter ore drop quantities based on their world position
+    /// </summary>
+    [HarmonyPatch(typeof(MovementController), "MovementHandler")]
+    public class MovementController_MovementHandler
+    {
+        static void Prefix(MovementController __instance)
+        {
+            if (KeyManager.GetButton((KeyCode)Enum.Parse(typeof(KeyCode), SlowDown.SlowKey)))
+            {
+                __instance.MovementForce = SlowDown.SlowedSpeed / 20f;
+                __instance.CharacterMaxSpeed = SlowDown.SlowedSpeed;
+            }
+            else
+            {
+                __instance.MovementForce = 0.2f;
+                __instance.CharacterMaxSpeed = 4f;
+            }
+        }
+    }
+
+
+}
---
+++ SlowDown/SlowDown.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Configuration;
+
+namespace SlowDown
+{
+    [BepInPlugin("SlowDown", "Slow Down Mod", "1.0.0.0")]
+    public class SlowDown : BaseUnityPlugin
+    {
+        private ConfigEntry<string> configSlowKey;
+        private ConfigEntry<float> configSlowedSpeed;
+
+        public static string SlowKey;
+        public static float SlowedSpeed;
+
+        public static void ModLog(string text)
+        {
+            UnityEngine.Debug.Log("[Slow Down] " + text);
+        }
+
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake()
+        {
+            HandleConfig();
+
+            ModLog("Successfully loaded Slow Down");
+            ModLog("Patching...");
+            var harmony = new Harmony("SlowDown");
+            harmony.PatchAll();
+            ModLog("Patched");
+        }
+
+        void HandleConfig()
+        {
+            configSlowKey = Config.Bind("General",   // The section under which the option is shown
+                                     "SlowKeyCode",  // The key of the configuration option in the configuration file
+                                     "LeftControl", // The default value
+                                     "The key to use to slow down - https://docs.unity3d.com/ScriptReference/KeyCode.html"); // Description of the option to show in the config file
+
+            SlowKey = configSlowKey.Value;
+
+            configSlowedSpeed = Config.Bind("General",   // The section under which the option is shown
+                                     "SlowedSpeed",  // The key of the configuration option in the configuration file
+                                     2f, // The default value
+                                     "The speed to slow down to (vanilla movement speed is 0.4) "); // Description of the option to show in the config file
+
+            SlowedSpeed = configSlowedSpeed.Value;
+        }
+
+    }
+}
